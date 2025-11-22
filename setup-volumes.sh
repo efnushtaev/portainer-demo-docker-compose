1b3bd1a9b1aaaec93ae4437a066c00de9c3d9895
Efim Nushtaev <enushtaev@alfabank.ru>
2025-11-22 15:29:13 +0300

    refactoring

--- setup-volumes.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Создаем временный контейнер для копирования файлов в том
-docker run --rm -v $(pwd)/mysql/initdb:/source -v mysql_init:/target alpine \
-  cp -r /source/. /target/
-
-# Проверяем что файлы скопировались
-docker run --rm -v mysql_init:/data alpine ls -la /data
